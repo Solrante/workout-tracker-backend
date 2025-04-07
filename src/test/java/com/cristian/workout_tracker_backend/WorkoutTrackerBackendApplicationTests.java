f60a427ce715c042352e7b03c811e1a6b9cf1311
Cristian <aecm.cristianrodriguez@gmail.com>
2025-04-07 17:41:26 +0200

    Commit inicial de proyecto

---
+++ src/test/java/com/cristian/workout_tracker_backend/WorkoutTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.workout_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
